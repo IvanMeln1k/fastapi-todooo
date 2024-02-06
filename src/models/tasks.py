e5a95dc8029090872cf285638614cc4e9a99fe74
Ivan Melnik <IvanMelnikV@ya.ru>
2024-02-06 13:47:08 +0300

    correct table tasks 2

--- src/models/tasks.py
+++ src/models/tasks.py
@@ -1,6 +1,7 @@
 from src.database import Base
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import text
 
 
 class Tasks(Base):
@@ -9,7 +10,7 @@ class Tasks(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
-    done: Mapped[bool] = mapped_column()
+    done: Mapped[bool] = mapped_column(server_default=text("false"))
 
     categories: Mapped[list["Categories"]] = relationship(
         back_populates="tasks",
